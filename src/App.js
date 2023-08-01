b6e62aed6751612ab33188e2e5de0e1f84c8d117
Austin <austinjbarrett98@gmail.com>
2023-08-01 10:48:33 -0400

    Updates project displays

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import tasteful from './images/tf_home.png'
+import astro from './images/astro_home.png'
 import jsx from './images/react.svg'
 import './App.css';
 import './tanuki.css'
@@ -91,7 +92,7 @@ const Box = () => {
       <div className="group">
         <div className="overlap-group">
           <p className="ghi-src-candidates">
-            src&nbsp;&nbsp;> candidates&nbsp;&nbsp;>&nbsp;&nbsp;<img src={jsx} style={{verticalAlign: 'middle'}}/>&nbsp;<a className='jsx-link' href='FILL RESUME HERE'>austin_barrett.jsx</a>
+            src&nbsp;&nbsp;> candidates&nbsp;&nbsp;>&nbsp;&nbsp;<img src={jsx} style={{verticalAlign: 'middle'}}/>&nbsp;<a className='jsx-link' href='https://drive.google.com/file/d/1RdkHBZPQ_WvrMr5j-zakAz13Y3er-waL/view'>austin_barrett.jsx</a>
           </p>
         </div>
       </div>
@@ -102,17 +103,41 @@ const Box = () => {
 const Dots = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={60}
-    height={16}
+    width={38}
+    height={10}
     fill="none"
     {...props}
   >
-    <circle cx={8} cy={8} r={8} fill="#787878" fillOpacity={0.5} />
-    <circle cx={8} cy={8} r={7.5} stroke="#2F2F2F" strokeOpacity={0.5} />
-    <circle cx={30} cy={8} r={8} fill="#787878" fillOpacity={0.5} />
-    <circle cx={30} cy={8} r={7.5} stroke="#2F2F2F" strokeOpacity={0.5} />
-    <circle cx={52} cy={8} r={8} fill="#787878" fillOpacity={0.5} />
-    <circle cx={52} cy={8} r={7.5} stroke="#2F2F2F" strokeOpacity={0.5} />
+    <path
+      fill="#6B7280"
+      fillOpacity={0.5}
+      d="M5.067 0c2.798 0 5.066 2.239 5.066 5s-2.268 5-5.066 5C2.268 10 0 7.761 0 5s2.268-5 5.067-5Z"
+    />
+    <path
+      stroke="#2F2F2F"
+      strokeOpacity={0.5}
+      d="M5.067.313C7.69.313 9.817 2.41 9.817 5c0 2.589-2.127 4.688-4.75 4.688-2.624 0-4.75-2.1-4.75-4.688 0-2.589 2.126-4.688 4.75-4.688Z"
+    />
+    <path
+      fill="#6B7280"
+      fillOpacity={0.5}
+      d="M19 0c2.798 0 5.067 2.239 5.067 5S21.798 10 19 10s-5.067-2.239-5.067-5S16.202 0 19 0Z"
+    />
+    <path
+      stroke="#2F2F2F"
+      strokeOpacity={0.5}
+      d="M19 .313c2.623 0 4.75 2.098 4.75 4.687 0 2.589-2.127 4.688-4.75 4.688S14.25 7.588 14.25 5c0-2.589 2.127-4.688 4.75-4.688Z"
+    />
+    <path
+      fill="#6B7280"
+      fillOpacity={0.5}
+      d="M32.933 0C35.731 0 38 2.239 38 5s-2.269 5-5.067 5-5.066-2.239-5.066-5 2.268-5 5.066-5Z"
+    />
+    <path
+      stroke="#2F2F2F"
+      strokeOpacity={0.5}
+      d="M32.933.313c2.624 0 4.75 2.098 4.75 4.687 0 2.589-2.126 4.688-4.75 4.688-2.623 0-4.75-2.1-4.75-4.688 0-2.589 2.127-4.688 4.75-4.688Z"
+    />
   </svg>
 )
 
@@ -125,7 +150,7 @@ function Nav(){
       <div className='title'>Full Stack Developer</div>
     </div>
     <div className='social-container'>
-      <div className='resume'><a className='link-hover'>Resume</a></div>
+      <div className='resume'><a className='link-hover' href='https://drive.google.com/file/d/1RdkHBZPQ_WvrMr5j-zakAz13Y3er-waL/view'>Resume</a></div>
       <div className='contact'><a className='link-hover'>Contact</a></div>
       <a href='https://www.linkedin.com/in/abarrettj/' title='My LinkedIn'>
         <LinkedIn/>
@@ -293,6 +318,46 @@ function Projects(){
           </div>
         </div>
         <div className='project-display-one'>
+        <div className='browser-top'>
+        <Dots/>
+        </div>
+        <div className='search-bar'>
+          <div class="nav-items">
+            <div class="flex ml-[20px] text-gray-500" style={{display: 'flex', paddingLeft: '20px', paddingRight: '20px'}}>
+              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" class="mt-1 mr-2 duration-300 ease-in-out rounded-full hover:text-gray-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path d="M872 474H286.9l350.2-304c5.6-4.9 2.2-14-5.2-14h-88.5c-3.9 0-7.6 1.4-10.5 3.9L155 487.8a31.96 31.96 0 0 0 0 48.3L535.1 866c1.5 1.3 3.3 2 5.2 2h91.5c7.4 0 10.8-9.2 5.2-14L286.9 550H872c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z">
+                </path>
+              </svg>
+              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" class="mt-1 mr-2 duration-300 ease-in-out hover:text-gray-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path d="M869 487.8L491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h-88.5c-7.4 0-10.8 9.2-5.2 14l350.2 304H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h585.1L386.9 854c-5.6 4.9-2.2 14 5.2 14h91.5c1.9 0 3.8-.7 5.2-2L869 536.2a32.07 32.07 0 0 0 0-48.4z">
+                </path>
+              </svg>
+              <svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 24 24" class="mt-[2px] mr-2 text-xl duration-300 ease-in-out hover:text-gray-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13.1459 11.0499L12.9716 9.05752L15.3462 8.84977C14.4471 7.98322 13.2242 7.4503 11.8769 7.4503C9.11547 7.4503 6.87689 9.68888 6.87689 12.4503C6.87689 15.2117 9.11547 17.4503 11.8769 17.4503C13.6977 17.4503 15.2911 16.4771 16.1654 15.0224L18.1682 15.5231C17.0301 17.8487 14.6405 19.4503 11.8769 19.4503C8.0109 19.4503 4.87689 16.3163 4.87689 12.4503C4.87689 8.58431 8.0109 5.4503 11.8769 5.4503C13.8233 5.4503 15.5842 6.24474 16.853 7.52706L16.6078 4.72412L18.6002 4.5498L19.1231 10.527L13.1459 11.0499Z" fill="currentColor">
+                </path>
+              </svg>
+              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="mt-1 mr-4 duration-300 ease-in-out hover:text-gray-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <g>
+                  <path fill="none" d="M0 0h24v24H0z">
+                  </path>
+                  <path d="M20 20a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-9H1l10.327-9.388a1 1 0 0 1 1.346 0L23 11h-3v9z">
+                  </path>
+                </g>
+              </svg>
+            </div>
+            <div class="flex w-full mr-4 bg-gray-800 rounded-full justify-evenly lg:px-4 laptop:px-0 xs:justify-center xs:w-5/5">
+
+              <a href="https://github.com/rfp2207boyz2men/frontendcapstone" target="_blank" class="browser-link"> https://tastefulapp.gitlab.io/tasteful/ </a>
+              <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="text-gray-500 top-[1px] mt-[3px] text-center duration-300 ease-in-out hover:text-gray-400 phone:hidden xs:hidden xs:text-xs" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                {/* <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon> */}
+              </svg>
+            </div>
+            {/* <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" class="w-10 mt-[3px] text-center text-gray-500 duration-300 ease-in-out hover:text-gray-400 phone:hidden xs:hidden" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+              <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z">
+              </path>
+            </svg> */}
+          </div>
+          </div>
           <img src={tasteful} className='display-one-img'/>
         </div>
       </div>
@@ -347,7 +412,42 @@ function Projects(){
             </div>
           </div>
         </div>
-        <div className='project-display-two'>
+        <div className='project-display-one'>
+        <div className='browser-top'>
+        <Dots/>
+        </div>
+        <div className='search-bar'>
+          <div class="nav-items">
+            <div class="flex ml-[20px] text-gray-500" style={{display: 'flex', paddingLeft: '20px', paddingRight: '20px'}}>
+              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" class="mt-1 mr-2 duration-300 ease-in-out rounded-full hover:text-gray-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path d="M872 474H286.9l350.2-304c5.6-4.9 2.2-14-5.2-14h-88.5c-3.9 0-7.6 1.4-10.5 3.9L155 487.8a31.96 31.96 0 0 0 0 48.3L535.1 866c1.5 1.3 3.3 2 5.2 2h91.5c7.4 0 10.8-9.2 5.2-14L286.9 550H872c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z">
+                </path>
+              </svg>
+              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" class="mt-1 mr-2 duration-300 ease-in-out hover:text-gray-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path d="M869 487.8L491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h-88.5c-7.4 0-10.8 9.2-5.2 14l350.2 304H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h585.1L386.9 854c-5.6 4.9-2.2 14 5.2 14h91.5c1.9 0 3.8-.7 5.2-2L869 536.2a32.07 32.07 0 0 0 0-48.4z">
+                </path>
+              </svg>
+              <svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 24 24" class="mt-[2px] mr-2 text-xl duration-300 ease-in-out hover:text-gray-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13.1459 11.0499L12.9716 9.05752L15.3462 8.84977C14.4471 7.98322 13.2242 7.4503 11.8769 7.4503C9.11547 7.4503 6.87689 9.68888 6.87689 12.4503C6.87689 15.2117 9.11547 17.4503 11.8769 17.4503C13.6977 17.4503 15.2911 16.4771 16.1654 15.0224L18.1682 15.5231C17.0301 17.8487 14.6405 19.4503 11.8769 19.4503C8.0109 19.4503 4.87689 16.3163 4.87689 12.4503C4.87689 8.58431 8.0109 5.4503 11.8769 5.4503C13.8233 5.4503 15.5842 6.24474 16.853 7.52706L16.6078 4.72412L18.6002 4.5498L19.1231 10.527L13.1459 11.0499Z" fill="currentColor">
+                </path>
+              </svg>
+              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="mt-1 mr-4 duration-300 ease-in-out hover:text-gray-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <g>
+                  <path fill="none" d="M0 0h24v24H0z">
+                  </path>
+                  <path d="M20 20a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-9H1l10.327-9.388a1 1 0 0 1 1.346 0L23 11h-3v9z">
+                  </path>
+                </g>
+              </svg>
+            </div>
+            <div class="flex w-full mr-4 bg-gray-800 rounded-full justify-evenly lg:px-4 laptop:px-0 xs:justify-center xs:w-5/5">
+              <a href="https://github.com/rfp2207boyz2men/frontendcapstone" target="_blank" class="browser-link"> https://gitlab.com/barrettAJ/astro/ </a>
+              <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="text-gray-500 top-[1px] mt-[3px] text-center duration-300 ease-in-out hover:text-gray-400 phone:hidden xs:hidden xs:text-xs" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+              </svg>
+            </div>
+          </div>
+          </div>
+          <img src={astro} className='display-one-img'/>
         </div>
       </div>
       <div className='title-row'>
