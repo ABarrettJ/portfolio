170c018a60422935275a2d3080954ec37a01e8ab
Austin Barrett <austinjbarrett98@gmail.com>
2024-05-14 17:17:01 -0400

    feat: updates nav resume

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -15,7 +15,7 @@ export default function Nav(){
         <div className='title'>Full Stack Developer</div>
       </div>
       <div className='social-container'>
-        <div className='resume'><a className='link-hover' href='https://drive.google.com/file/d/1jjqUpejs8H7VZvrM_3v46-31ExMxcEpu/view'>Resume</a></div>
+        <div className='resume'><a className='link-hover' href='https://drive.google.com/file/d/1krDAeMbtWOKAa_1byrHQjH_YSXhF-uaO/preview'>Resume</a></div>
         <div className='contact'><a onClick={scrollToSocialContainer} className='link-hover'>Contact</a></div>
         {/* 2232 */}
         <a href='https://www.linkedin.com/in/abarrettj/' title='My LinkedIn'>
